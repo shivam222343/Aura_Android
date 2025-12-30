0c9b987d65f0db1e942b672f646ca56c0f5bcc35
Shivam <Shivamdombe1@gmail.com>
2025-12-31 02:50:03 +0530

    Fix middleware import path in users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getBirthdaysToday } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 // Get birthdays today
 router.get('/birthdays-today', protect, getBirthdaysToday);
