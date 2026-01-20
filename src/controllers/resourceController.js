4d6b6d86e00782001a405a114d11b79c6f5e0a9a
Shivam <Shivamdombe1@gmail.com>
2026-01-21 01:57:58 +0530

    Uplaod resources

--- src/controllers/resourceController.js
+++ src/controllers/resourceController.js
@@ -1,5 +1,5 @@
 const EventResource = require('../models/EventResource');
-const cloudinary = require('../config/cloudinary');
+const { cloudinary } = require('../config/cloudinary');
 
 exports.getResources = async (req, res) => {
     try {
